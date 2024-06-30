180d411af9ccb403a42a719fb6ce9fcc5ef881b6
Caner Sivri <canersivri@protonmail.com>
2024-06-30 16:29:25 +0300

    Add files via upload

---
+++ primenumberchecker.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int number;
+    int count=0;
+    
+    printf("Please enter a number: ");
+    scanf("%d",&number);
+
+    for(int i=1;i<=number;i++){
+        if(number % i == 0){
+            count++;
+        }
+    }
+    if(count<=2){
+        printf("Your number is prime\n");
+    }
+    else{
+        printf("Your number isn't prime\n");
+    }
+    return 0;
+}
